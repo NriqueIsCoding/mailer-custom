04ad3eff16761e4c381815d12df2e6206219bfa0
nrique <eesta83@gmail.com>
2018-01-30 11:43:03 -0500

    sending email passed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,12 @@
 class UserMailer < ApplicationMailer
   default from: "hoorrayfun123@gmail.com"
 
-  def welcome_email()
+  puts "**UserMailer Initialized***"
+
+  def welcome_email(user)
     p "***Welcome email action fired***"
+    @user = user
+    mail(to: @user.email, subject: "Welcome!!!")
   end
 
 
